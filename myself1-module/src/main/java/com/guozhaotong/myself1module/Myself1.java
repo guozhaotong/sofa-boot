b779e9c7f3353a097168fd8e7bc1b99f7e9c382b
郭朝彤 <guochaotong@corp.netease.com>
2018-08-08 09:34:48 +0800

    删除多余输出

---
+++ myself1-module/src/main/java/com/guozhaotong/myself1module/Myself1.java
@@ -0,0 +1,10 @@
+package com.guozhaotong.myself1module;
+
+import com.guozhaotong.Guo;
+
+public class Myself1 {
+    public String output(){
+        Guo guo = new Guo();
+        return guo.sayHello();
+    }
+}
---
+++ myself2-module/src/main/java/com/guozhaotong/myself2module/Myself2.java
@@ -0,0 +1,10 @@
+package com.guozhaotong.myself2module;
+
+import com.guozhaotong.Guo;
+
+public class Myself2 {
+    public String output(){
+        Guo guo = new Guo();
+        return guo.outputPerson();
+    }
+}
